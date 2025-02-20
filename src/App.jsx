1592aa0ed5f370a4371bd18d63c750937f816709
Prem Kumar <prem68265@gmail.com>
2025-02-20 21:02:56 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+
 
 import './App.scss'
 
@@ -6,7 +8,29 @@ function App() {
 
   return (
 <>
-<h1>Home Page</h1>
+<div className="container p-4 m-0 homecontact">
+  <div className="row p-0 m-0 parentheight">
+    <div className="col-lg-8 col-md-6 pe-4 d-flex flex-column justify-content-center parentheight" >
+<h4>heading 001</h4>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates magni deleniti facilis? Repellendus doloremque molestiae et asperiores dolorum excepturi debitis dolor inventore, possimus, veritatis sequi quos enim harum ut dolore.</p>
+<button className='butn'>Button</button>
+
+    </div>
+    <div className="col-lg-4 col-md-6 d-flex flex-column justify-content-center parentheight">
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+    </div>
+
+  </div>
+</div>
 </>
   )
 }

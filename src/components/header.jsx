f2e7eba7c72bd6546c806b948fc5ec4565b47262
Prem Kumar <prem68265@gmail.com>
2025-02-20 20:30:35 +0530

    navbar color change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
-                            className="me-auto my-2 my-lg-0"
+                            className="ms-auto my-2 my-lg-0"
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
@@ -23,7 +23,7 @@ export default function Header() {
                             <Nav.Link href="#action2">Link</Nav.Link>
                             <NavDropdown title="Link" id="navbarScrollingDropdown">
                                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action4">
+                                <NavDropdown.Item href="#action4" >
                                     Another action
                                 </NavDropdown.Item>
                                 <NavDropdown.Divider />
@@ -33,6 +33,12 @@ export default function Header() {
                             </NavDropdown>
                             <Nav.Link href="#" disabled>
                                 Link
+                            </Nav.Link>
+                            <Nav.Link href="#" disabled>
+                               Contact
+                            </Nav.Link>
+                            <Nav.Link href="#" disabled>
+                              Terms & Conditions
                             </Nav.Link>
                         </Nav>
                        
